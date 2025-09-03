34376336536e5e1ff8068c39ae591552dba3f8da
BrunoMendes <brunomendes.ccii@gmail.com>
2025-09-03 15:37:52 +0100

    Update agendamentos.sql

--- agendamentos.sql
+++ agendamentos.sql
@@ -1,11 +1,17 @@
--- Script opcional para criar a tabela (caso desejes via DB tool)
-CREATE TABLE IF NOT EXISTS agendamentos (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  nomeEvento TEXT,
-  data TEXT,
-  horaInicio TEXT,
-  horaFim TEXT,
-  sala TEXT,
+CREATE TABLE agendamentos (
+  id SERIAL PRIMARY KEY,
+  nome_evento VARCHAR(100),
+  data DATE,
+  hora_inicio TIME,
+  hora_fim TIME,
+  sala VARCHAR(100),
+  num_mec VARCHAR(20),
+  nome_req VARCHAR(100),
+  servico_req VARCHAR(100),
+  email_req VARCHAR(100),
+  contacto_req VARCHAR(20),
   participantes INTEGER,
-  observacoes TEXT
+  observacoes TEXT,
+  tipo_evento VARCHAR(100),
+  recursos TEXT
 );
